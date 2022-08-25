e98bfc1e08949e899fc0161a02d0c111f54e0734
kairi003 <kairi.satellite@gmail.com>
2022-08-25 20:23:04 +0900

    [update] add role check

--- namingway.py
+++ namingway.py
@@ -12,6 +12,8 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command()
+@commands.guild_only()
+@commands.has_role('Cecil')
 async def nick(ctx: commands.Context, target:discord.Member, new_name: str):
     print(ctx.author, target, new_name)
     old_name = target.display_name
@@ -19,6 +21,8 @@ async def nick(ctx: commands.Context, target:discord.Member, new_name: str):
     await ctx.send(f"<@{target.id}> renamed by <@{ctx.author.id}> ({old_name} -> {new_name})")
 
 @bot.command()
+@commands.guild_only()
+@commands.has_role('Cecil')
 async def ybb(ctx: commands.Context, _target: Optional[discord.Member], _new_name: Optional[str]):
     target = _target if _target else ctx.author
     yubaba = '湯婆婆' if ctx.author == target else f'<@{ctx.author.id}>'
@@ -27,6 +31,11 @@ async def ybb(ctx: commands.Context, _target: Optional[discord.Member], _new_nam
     await target.edit(nick=new_name)
     await ctx.send(f"{yubaba}「フン。{old_name}というのかい。贅沢な名だねぇ。\n"
         f"今からお前の名前は{new_name}だ。いいかい、{new_name}だよ。分かったら返事をするんだ、<@{target.id}>!!」")
+
+@nick.error
+@ybb.error
+async def info_error(ctx: commands.Context, error: Exception):
+    await ctx.send(str(error))
 
 @bot.event
 async def on_ready():
